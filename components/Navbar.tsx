7692942c9d0d8a840c256561287d13befb7e611f
SunilDy <sunildyofficial@gmail.com>
2023-02-15 15:49:48 +0530

    fixed navbar overflow-x

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,8 +23,8 @@ const Navbar = () => {
     <nav
       className={`
       flex items-center lg:gap-x-2
-      xsm:py-2 lg:py-8 
-      xsm:px-2 md:px-16 lg:px-20 xl:px-32 
+      xsm:py-0 lg:py-4 
+      xsm:px-6 md:px-16 lg:px-20 xl:px-32 
       justify-between text-white backdrop-blur-3xl z-50 sticky top-0 border-b-2 border-white border-opacity-10`}
     >
       {/* Primary Links */}
@@ -68,52 +68,52 @@ const Navbar = () => {
         />
       </div>
       {/* User */}
-      <div className={`basis-1/4 justify-self-start xsm:collapse lg:visible`}>
-        <Link href={"/login"}>
+      <div className={`basis-1/4 justify-self-end flex items-center`}>
+        <div className="xsm:collapse xsm:w-0 lg:visible lg:w-full">
           {session && session.user ? (
-            <h1
-              className={`xsm:text-md md:text-lg lg:text-xl  basis-1/4 text-right`}
-            >
-              Hi, {session?.user.name}
-            </h1>
+            <Link href={"/"}>
+              <h1 className={`xsm:text-md md:text-lg lg:text-xl text-right`}>
+                Hi, {session?.user.name}
+              </h1>
+            </Link>
           ) : (
-            <h1
-              className={`xsm:text-md md:text-lg lg:text-xl  basis-1/4 text-right`}
-            >
-              Login
-            </h1>
+            <Link href={"/login"}>
+              <h1 className={`xsm:text-md md:text-lg lg:text-xl text-right`}>
+                Login
+              </h1>
+            </Link>
           )}
-        </Link>
-      </div>
-      {/* Menu */}
-      <div className="xsm:visible lg:collapse flex-1">
-        <Menu
-          menuButton={
-            <MenuButton className="bg-white rounded px-2 m-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="hotpink"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 9a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9zm0 6.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </MenuButton>
-          }
-          transition
-          menuStyle={{
-            backgroundColor: "#ffffff97",
-            color: "white",
-          }}
-        >
-          <MenuItem>Authors</MenuItem>
-          <MenuItem>Search</MenuItem>
-          <MenuItem>User Profile</MenuItem>
-        </Menu>
+        </div>
+        {/* Menu */}
+        <div className="xsm:visible xsm:w-full lg:collapse lg:w-0 flex-1 text-right">
+          <Menu
+            menuButton={
+              <MenuButton className="bg-white rounded px-2 m-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="hotpink"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 9a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9zm0 6.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </MenuButton>
+            }
+            transition
+            menuStyle={{
+              backgroundColor: "#ffffff97",
+              color: "white",
+            }}
+          >
+            <MenuItem>Authors</MenuItem>
+            <MenuItem>Search</MenuItem>
+            <MenuItem>User Profile</MenuItem>
+          </Menu>
+        </div>
       </div>
     </nav>
   );
